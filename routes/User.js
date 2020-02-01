8ab0ee33d1f15811efd0f404f1f982f66538bed9
danyentezari <dany.entezari@gmail.com>
2020-02-01 15:23:09 +0400

    Added bcrypt.

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const UserModel = require('../models/User.js')
 
@@ -11,14 +12,32 @@ router.post(
             'firstname': req.body.firstname,
             'lastname': req.body.lastname,
             'email': req.body.email,
-            'password': req.body.password,
+            'password': req.body.password, // myPass213, for example.
             'occupation': req.body.occupation
         }
 
         const theUserModel = new UserModel(formdata);
-        theUserModel.save();
 
-        res.send("User registration complete")
+        // Step 1. Generate a salt
+        bcrypt.genSalt(
+            (err, salt)=>{
+                // Step 2. Generate the hashed password
+                bcrypt.hash(
+                    formdata.password, //password
+                    salt, //generated salt
+                    (err, hashedPassword)=>{
+
+                        // Step 3. Replace the password value in formdata
+                        theUserModel.password = hashedPassword; //myPass123 is now $2b$10$H5IXXYWXx
+
+                        // Step 4. Save to database
+                        theUserModel.save();
+                        res.send("User registration complete")
+
+                    }// how we handle the new hased password
+                )
+            }
+        )
     }
 )
 
