4adb4f65089ae39a1862d4841409a3a60f2aa1ec
danyentezari <dany.entezari@gmail.com>
2020-02-15 09:50:00 +0400

    Added create route.

--- routes/Feed.js
+++ routes/Feed.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const router = express.Router();
-// const FeedModel = require('../models/Feed.js');
+const FeedModel = require('../models/Feed.js');
+
+router.post(
+    '/create',
+    (req, res)=>{
+        const formdata = {
+            description: req.body.description,
+            image: req.body.image,
+            hashtags: req.body.hashtags,
+        }
+
+        const newFeedModel = new FeedModel(formdata);
+        newFeedModel.save();
+        res.send('Feed is created');
+    }
+);
 
 router.get(
-    '/create',// http://www.myapp.com/feed/create
+    '/all',// http://www.myapp.com/feed/create
     (req, res)=>{
-        res.send("Here are your feeds");
+
+        FeedModel
+        .find()
+        .then((results)=>{
+            res.json(
+                {
+                    msg:'here are your feeds', 
+                    results: results
+                }
+            );
+        })   
     }
-) 
+);
+
 
 
 module.exports = router;
